c96067eaf95609f26b69d15a3d0846c790ae882e
Sayanaann <alexsayana@gmail.com>
2022-11-29 14:35:58 +0530

    services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,38 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddToDoComponent } from './add-to-do/add-to-do.component';
 import { ViewAllComponent } from './view-all/view-all.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
+const myRoute:Routes=[
+  {
+    path :"",
+    component :AddToDoComponent
+  },
+  {
+    path :"viewall",
+    component :ViewAllComponent
+  }
+
+
+]
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AddToDoComponent,
-    ViewAllComponent
+    ViewAllComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute),
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
